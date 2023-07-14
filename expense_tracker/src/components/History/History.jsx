25b2ebd65c37b4327f53d87f4def3778f2f1e142
Abraham-Ogbumah <ab.ogbumah@gmail.com>
2023-07-14 11:46:48 +0100

    update to history component

--- expense_tracker/src/components/History/History.jsx
+++ expense_tracker/src/components/History/History.jsx
@@ -2,7 +2,14 @@
 
 const History = () => {
   return (
-    <div>History</div>
+    <>
+      <h3>History</h3>
+      <hr></hr>
+      <div>
+        <p>Groceries</p>
+        <p>$50</p>
+      </div>
+    </>
   )
 }
 
