e8c15be432a1e0b255bbd4e3237ecedd811fa8c7
Abraham-Ogbumah <ab.ogbumah@gmail.com>
2023-07-18 11:25:16 +0100

    update to transaction section

--- expense_tracker/src/components/AddTransaction/AddTransaction.jsx
+++ expense_tracker/src/components/AddTransaction/AddTransaction.jsx
@@ -5,17 +5,21 @@ const AddTransaction = () => {
     <>
       <h3 className="sub-header">Add Transaction</h3>
       <hr></hr>
+      <div className="form">
       <form>
-        <label htmlFor="title">
-          <input type="text" placeholder="Income / Expense"/>
-        </label>
+        <div>
+          <label htmlFor="title" className="label">Text</label>
+          <input id="title" name="name" type="text" placeholder="Enter description" className="input"/>
+        </div>
         <br/>
-        <label htmlFor="amount">
-          <input type="text" placeholder="Income / Expense"/>
-        </label>
+        <div>
+          <label htmlFor="amount" className="label">Amount</label>
+          <input type="text" placeholder="Enter Amount" className="input"/>
+        </div>
         <br />
-        <input type="submit" value="Submit" />
+        <input type="submit" value="Add Transaction" className="submit"/>
       </form>
+      </div>
     </>
   )
 }
